7e5d3f482178cf9a97a85a833459f18b13e9e514
amr4356 <amrooo73321@gmail.com>
2024-08-06 08:50:38 +0300

    421. Adding a Custom Component To A Layout

--- app/meals/page.js
+++ app/meals/page.js
@@ -4,10 +4,7 @@ export default function MealsPage(){
     return (
         <>
             <h1>Meals Page</h1>
-            <div>
-                <Link href="meal1">meal 1</Link>
-                <Link href="meal2">meal 2</Link>
-            </div>
+
             <div>
                 <Link href="meals/share">Share</Link>
             </div>
